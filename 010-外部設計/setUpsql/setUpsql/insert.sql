355c4351b5524220478bddf2b69fe99b561c2729
miura <ru.miura@kkdis.onmicrosoft.com>
2025-09-08 10:42:45 +0900

    setupSQL

---
+++ 010-外部設計/setUpsql/setUpsql/insert.sql
@@ -0,0 +1,22 @@
+--カテゴリーテーブルのデータ初期データ
+INSERT INTO categories (category_id, category_name) VALUES (default,'帽子');
+INSERT INTO categories (category_id, category_name) VALUES (default,'鞄');
+INSERT INTO categories (category_id, category_name) VALUES (default,'アクセサリー');
+
+--会員テーブルの初期データ
+--password(tarou111)
+INSERT INTO members (email, password, member_name, address, is_withdrawal) VALUES ('Tarou@ec.com','03f044b670d4fa5c06544b3120ae7ac13d98da962396a6907741f3ed12117687','太郎','東京都渋谷区1-1-1',default);
+--password(hanako222)
+INSERT INTO members (email, password, member_name, address, is_withdrawal) VALUES ('Hanako@ec.com','edbbe43789981853482f12a24ee557d99c166947368be7e9f69504ee45965b28','花子','東京都練馬区2-2-2',default);
+--password(len333)
+INSERT INTO members (email, password, member_name, address, is_withdrawal) VALUES ('Len@ec.com','34f42475ede8ba1bb9772d79d0ba388447b16b56b81caea9e15a0d4e5b54a00a','蓮','東京都千代田区3-3-3',default);
+
+--商品テーブルの初期データ
+INSERT INTO items (item_name, color, manufacturer, price, stock, category_id, recommended) VALUES ('おうかん','金色','ロイヤル商店',20000.00,10,1,default);
+INSERT INTO items (item_name, color, manufacturer, price, stock, category_id, recommended) VALUES ('おおきな帽子','黒色','ノーマル商店',4000.00,250,1,default);
+INSERT INTO items (item_name, color, manufacturer, price, stock, category_id, recommended) VALUES ('良質なトランク','茶色','ロイヤル商店',15600.00,20,2,TRUE);
+INSERT INTO items (item_name, color, manufacturer, price, stock, category_id, recommended) VALUES ('錆びれたリング','銀色','怪しいお店',250.00,66,3,default);
+
+SELECT * FROM categories;
+SELECT * FROM members;
+SELECT * FROM items;
---
+++ 010-外部設計/setUpsql/setUpsql/table.sql
@@ -0,0 +1,82 @@
+DROP TABLE IF EXISTS members CASCADE;
+DROP TABLE IF EXISTS categories CASCADE;
+DROP TABLE IF EXISTS items CASCADE;
+DROP TABLE IF EXISTS carts CASCADE ;
+DROP TABLE IF EXISTS purchases CASCADE;
+DROP TABLE IF EXISTS purchase_details CASCADE;
+DROP SEQUENCE IF EXISTS members_seq;
+DROP SEQUENCE IF EXISTS categories_seq;
+DROP SEQUENCE IF EXISTS items_seq;
+DROP SEQUENCE IF EXISTS purchases_seq;
+
+--カテゴリIDシーケンス
+CREATE SEQUENCE categories_seq;
+--会員番号シーケンス
+CREATE SEQUENCE members_seq START 10;
+--商品IDシーケンス
+CREATE SEQUENCE items_seq;
+--購入IDシーケンス
+CREATE SEQUENCE purchases_seq;
+
+--カテゴリテーブル
+CREATE TABLE categories(
+	category_id INTEGER PRIMARY KEY DEFAULT nextval('categories_seq'),
+	category_name VARCHAR(135) NOT NULL
+);
+
+--会員テーブル
+CREATE TABLE members(
+	member_number INTEGER PRIMARY KEY DEFAULT nextval('members_seq'),
+   	email VARCHAR(254) NOT NULL UNIQUE,
+	password VARCHAR(64) NOT NULL,
+	member_name VARCHAR(60) NOT NULL,
+	address VARCHAR(180) NOT NULL,
+	is_withdrawal BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+--商品テーブル
+CREATE TABLE items(
+	item_id INTEGER PRIMARY KEY DEFAULT nextval('items_seq'),
+	item_name VARCHAR(135) NOT NULL,
+	color VARCHAR(20) NOT NULL,
+	manufacturer VARCHAR(137) NOT NULL,
+	price NUMERIC(10,2) NOT NULL,
+	stock INTEGER NOT NULL DEFAULT 0,
+	category_id INTEGER NOT NULL,
+	recommended BOOLEAN NOT NULL DEFAULT FALSE,
+	FOREIGN KEY (category_id) REFERENCES categories(category_id)
+);
+
+--カートテーブル
+CREATE TABLE carts(
+	member_number INTEGER,
+	item_id INTEGER,
+	quantity INTEGER NOT NULL,
+	FOREIGN KEY (member_number) REFERENCES members(member_number), 
+	FOREIGN KEY (item_id) REFERENCES items(item_id), 
+	PRIMARY KEY (member_number,item_id)
+);
+
+--	購入テーブル
+CREATE TABLE purchases(
+	purchase_id INTEGER PRIMARY KEY DEFAULT nextval('purchases_seq'),
+	member_number INTEGER NOT NULL,
+	purchase_date DATE NOT NULL,
+	delivery_address VARCHAR(180) NOT NULL,
+	payment VARCHAR(20) NOT NULL,
+	cancel_flag BOOLEAN NOT NULL DEFAULT FALSE,
+	cancel_date DATE DEFAULT NULL,
+	FOREIGN KEY (member_number) REFERENCES members(member_number)
+);
+
+--購入詳細テーブル
+CREATE TABLE purchase_details(
+	purchase_id INTEGER,
+	item_id INTEGER,
+	quantity INTEGER NOT NULL,
+	FOREIGN KEY (purchase_id) REFERENCES purchases(purchase_id), 
+	FOREIGN KEY (item_id) REFERENCES items(item_id), 
+	PRIMARY KEY (purchase_id,item_id)
+);
+
+
