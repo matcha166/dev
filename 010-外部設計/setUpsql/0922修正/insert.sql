f8a0e3e3b9abdbd8961c09dfc26cd0d9d5e9a320
miura <ru.miura@kkdis.onmicrosoft.com>
2025-10-03 17:34:28 +0900

    クラス仕様書更新いたしました。お手すきの際にご確認のほどよろしくお願いいたします。

--- 010-外部設計/setUpsql/0922修正/insert.sql
+++ 010-外部設計/setUpsql/0922修正/insert.sql
@@ -3,9 +3,9 @@ INSERT INTO categories (category_id, category_name) VALUES (default,'帽子');
 INSERT INTO categories (category_id, category_name) VALUES (default,'鞄');
 
 --カラーテーブルのデータ初期データ
-INSERT INTO colors VALUES (1,'金色');
-INSERT INTO colors VALUES (2,'黒色');
-INSERT INTO colors VALUES (3,'茶色');
+INSERT INTO colors (color_id, color_name) VALUES (default,'金色');
+INSERT INTO colors (color_id, color_name) VALUES (default,'黒色');
+INSERT INTO colors (color_id, color_name) VALUES (default,'茶色');
 
 --会員テーブルの初期データ
 --password(tarou111)
@@ -16,10 +16,10 @@ INSERT INTO members (member_id, password, member_name, address, is_withdrawal) V
 INSERT INTO members (member_id, password, member_name, address, is_withdrawal) VALUES ('Len@ec.com','34f42475ede8ba1bb9772d79d0ba388447b16b56b81caea9e15a0d4e5b54a00a','蓮','東京都千代田区3-3-3',default);
 
 --商品テーブルの初期データ
-INSERT INTO items (item_name, color_id, manufacturer, price, stock, category_id, recommended) VALUES ('おうかん',1,'ロイヤル商店',20000.00,10,1,default);
-INSERT INTO items (item_name, color_id, manufacturer, price, stock, category_id, recommended) VALUES ('おおきな帽子',2,'ノーマル商店',4000.00,250,1,default);
-INSERT INTO items (item_name, color_id, manufacturer, price, stock, category_id, recommended) VALUES ('良質なトランク',3,'ロイヤル商店',15600.00,20,2,TRUE);
-INSERT INTO items (item_name, color_id, manufacturer, price, stock, category_id, recommended) VALUES ('怪しい鞄',2,'怪しいお店',250.00,66,2,default);
+INSERT INTO items (item_name, color_id, manufacturer, price, stock, category_id, recommended) VALUES ('おうかん',1,'ロイヤル商店',20000,10,1,default);
+INSERT INTO items (item_name, color_id, manufacturer, price, stock, category_id, recommended) VALUES ('おおきな帽子',1,'ノーマル商店',4000,250,1,default);
+INSERT INTO items (item_name, color_id, manufacturer, price, stock, category_id, recommended) VALUES ('良質なトランク',2,'ロイヤル商店',15600,20,2,TRUE);
+INSERT INTO items (item_name, color_id, manufacturer, price, stock, category_id, recommended) VALUES ('怪しい鞄',2,'怪しいお店',250,66,2,default);
 
 SELECT * FROM categories;
 SELECT * FROM members;
